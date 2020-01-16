b723a0c3fa2fe979e283dba1422d54be99865448
nicolasschulle <54943262+nicolasschulle@users.noreply.github.com>
2020-01-16 16:43:19 +0100

    test
    test

--- src/main/java/com/kek/terminal/Bus.java
+++ src/main/java/com/kek/terminal/Bus.java
@@ -1,4 +1,26 @@
 package com.kek.terminal;
 
 public class Bus {
+
+	public Bustype bustype;
+	public Capacity capacity;
+	public Comforttype comforttype;
+
+Bus(){
+	bustype = new Bustype();
+	capacity = new Capacity();
+	comforttype = new Comforttype();
+	
 }
+
+public void getbustype(){
+	return bustype();}
+	
+public void getcapacity(){
+	return capacity();}
+	
+public void getcomforttype(){
+	return comforttype();}
+
+}
+
